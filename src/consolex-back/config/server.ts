7ae707188f25db1f5af8bc2f617675494cfa2bb2
xdadev37 <pouriasa37@gmail.com>
2022-09-19 01:49:35 +0430

    PUBLIC_URL added;

--- src/consolex-back/config/server.ts
+++ src/consolex-back/config/server.ts
@@ -1,5 +1,6 @@
 export default ({ env }) => ({
   host: env("HOST", "https://strapi.consolex.shop"),
+  url: env("PUBLIC_URL", "https://strapi.consolex.shop"),
   port: env.int("PORT", 1337),
   app: {
     keys: ["4696048pouria"],
