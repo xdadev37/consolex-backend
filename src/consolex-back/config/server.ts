33777c76739f41f04aceac29adb1b79a0234636c
xdadev37 <pouriasa37@gmail.com>
2022-09-19 01:30:06 +0430

    optimzing;

--- src/consolex-back/config/server.ts
+++ src/consolex-back/config/server.ts
@@ -1,5 +1,5 @@
 export default ({ env }) => ({
-  host: env("HOST", "0.0.0.0"),
+  host: env("HOST", "https://strapi.consolex.shop"),
   port: env.int("PORT", 1337),
   app: {
     keys: ["4696048pouria"],
